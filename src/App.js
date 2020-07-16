842e35966eeb8e46504b5e58ccfa80c03d305bd9
dinh2350 <hao9x0159@gmail.com>
2020-07-16 12:02:32 +0700

    aoo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header';
-import BaiTapLayout from './Components/BaiTapLayout/BaiTapLayout';
-import BaiTapLayout2 from './Components/BaiTapLayout2/BaiTapLayout2';
-import DataBinding from './DataBinding/DataBinding';
-import EventBinding from './EventBinding/EventBinding';
-import RenderWithState from './RenderWithState/RenderWithState';
-import BaiTapChonXe from './RenderWithState/BaiTapChonXe';
-import RenderWithMap from './RenderWithState/RenderWithMap/RenderWithMap';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header";
+import BaiTapLayout from "./Components/BaiTapLayout/BaiTapLayout";
+import BaiTapLayout2 from "./Components/BaiTapLayout2/BaiTapLayout2";
+import DataBinding from "./DataBinding/DataBinding";
+import EventBinding from "./EventBinding/EventBinding";
+import RenderWithState from "./RenderWithState/RenderWithState";
+import BaiTapChonXe from "./RenderWithState/BaiTapChonXe";
+import RenderWithMap from "./RenderWithState/RenderWithMap/RenderWithMap";
 
 function App() {
   return (
     <div className="App">
-        {/* <BaiTapLayout /> */}
-        {/* <BaiTapLayout2 /> */}
-        {/* <DataBinding /> */}
-        {/* <EventBinding /> */}
-        {/* <RenderWithState /> */}
-        {/* <BaiTapChonXe /> */}
-        <RenderWithMap />
+      {/* <BaiTapLayout /> */}
+      {/* <BaiTapLayout2 /> */}
+      {/* <DataBinding /> */}
+      {/* <EventBinding /> */}
+      {/* <RenderWithState /> */}
+      {/* <BaiTapChonXe /> */}
+      {/* <RenderWithMap /> */}
     </div>
   );
 }
